52a35fb612fbde1d943410249a2c88037b2f0c13
chintan <chintan@gmail.com>
2025-12-24 10:07:57 +0530

    basic

---
+++ 001_Basic/demo.py
@@ -0,0 +1,34 @@
+# print("Hello")
+
+# print('Hello')
+
+# print("""
+# fdfdd
+#       dfdf
+#       dfdf
+#       fdsfdf
+# """)
+
+# He\"llo
+
+# print('He"llo')
+# print("He'llo")
+
+# print("He\\\"llo")
+# print("He\tllo \n pyth\bon")
+
+# print(r"He\tllo \n pyth\bon")
+
+# print("Hello",end="")
+# print("Python")
+
+# print("Python","Java","Php","Android",sep=" | ")
+
+name = "abc"
+email = "abc@gmail.com"
+
+# print("my name is {1} and email is {0}".format(name,email))
+
+print(f"my name is {name} and email is {email}")
+
+
